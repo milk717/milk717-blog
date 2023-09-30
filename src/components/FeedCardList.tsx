e2b57895a00f72b0684964760dcd3f59d50c9760
milk717 <robolindasoo@gmail.com>
2023-09-30 23:29:48 +0900

    :bug: FeedCardList 키설정 오류 수정

--- src/components/FeedCardList.tsx
+++ src/components/FeedCardList.tsx
@@ -5,16 +5,16 @@ const FeedCardList = ({data}: {data: Queries.BlogPostListQuery}) => {
     const posts = data.allMarkdownRemark?.edges;
     return (
         <div className="feed-card-list">
-            {posts?.map(({node: {frontmatter}}) => (
-                <div className="feed-card">
+            {posts?.map(({node}) => (
+                <div className="feed-card" key={node.id}>
                     <img
                         src="https://post-phinf.pstatic.net/MjAyMjExMTdfNzYg/MDAxNjY4NjUwMTQxMzU2.KOS5Iab6G0-88otScR2vsaZmoimqN8v-b5QiMhSkj5og.qsrQszwww-AZFP32Vs0gfJpqYp4tWpaXQYRupweSlOMg.JPEG/1633c62db4f2af416b327f14ce3c3100.jpg?type=w800_q75"
                         alt="썸네일 이미지"
                     />
                     <div className="text-area">
-                        <i>{frontmatter?.category}</i>
-                        <h2>{frontmatter?.title}</h2>
-                        <p>{frontmatter?.date}</p>
+                        <i>{node.frontmatter?.category}</i>
+                        <h2>{node.frontmatter?.title}</h2>
+                        <p>{node.frontmatter?.date}</p>
                     </div>
                 </div>
             ))}
