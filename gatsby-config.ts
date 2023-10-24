a103716705370d1d92e320436a68b611e5baf5e4
milk717 <robolindasoo@gmail.com>
2023-10-25 01:19:39 +0900

    :green_heart: ga 플러그인 옵션 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -61,9 +61,6 @@ const config: GatsbyConfig = {
             resolve: `gatsby-plugin-google-gtag`,
             options: {
                 trackingIds: [process.env.GA_TRACKING_ID],
-                pluginConfig: {
-                    head: false,
-                },
             },
         },
     ],
