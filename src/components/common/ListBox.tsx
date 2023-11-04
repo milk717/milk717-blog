c28ff95be2bf16d31eeae6eb94c331ca87d6abad
milk717 <robolindasoo@gmail.com>
2023-11-04 13:50:05 +0900

    :recycle: tag -> listbox로 변경하고 다른 곳에서도 사용할 수 있도록 리스트를 받도록 변경

---
+++ src/components/common/ListBox.tsx
@@ -0,0 +1,60 @@
+import {Box} from './Box';
+import {css} from '@emotion/react';
+import React, {FC} from 'react';
+
+type Props = {
+    title: string;
+    list: {text: string; subText: string}[];
+};
+
+export const ListBox: FC<Props> = ({title, list}) => {
+    return (
+        <Box
+            style={css`
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                row-gap: 1rem;
+                padding: 1.5rem;
+                border-radius: 2.5rem;
+            `}>
+            <h3
+                css={css`
+                    text-align: center;
+                    font-size: 1.125rem;
+                `}>
+                {title}
+            </h3>
+            <div
+                css={css`
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: flex-start;
+                    row-gap: 0.5rem;
+                    flex-grow: 1;
+                `}>
+                {list.map(({text, subText}) => (
+                    <div
+                        css={css`
+                            display: flex;
+                            justify-content: space-between;
+                            align-items: center;
+                        `}>
+                        <p
+                            css={css`
+                                font-size: 1rem;
+                            `}>
+                            {text}
+                        </p>
+                        <p
+                            css={css`
+                                font-size: 0.8rem;
+                            `}>
+                            {subText}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </Box>
+    );
+};
--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -2,8 +2,9 @@ import {css} from '@emotion/react';
 import {theme} from '../../styles/theme';
 import {Profile} from './Profile';
 import {Search} from './Search';
-import {Tags} from './Tags';
 import React from 'react';
+import {ListBox} from './ListBox';
+import {graphql, useStaticQuery} from 'gatsby';
 
 export const Sidebar = () => {
     return (
@@ -21,7 +22,21 @@ export const Sidebar = () => {
             `}>
             <Profile />
             <Search />
-            <Tags />
+            <ListBox
+                title={'Tags'}
+                list={
+                    useStaticQuery(graphql`
+                        query BlogTagsList {
+                            allMarkdownRemark {
+                                group(field: {frontmatter: {tags: SELECT}}) {
+                                    text: fieldValue
+                                    subText: totalCount
+                                }
+                            }
+                        }
+                    `).allMarkdownRemark.group
+                }
+            />
         </div>
     );
 };
