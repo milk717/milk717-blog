94a5cef0c3452fecfaf54ed2a614300f43fdc5d1
Sumin <robolindasoo@gmail.com>
2023-11-24 22:20:22 +0900

    :sparkles: 태그 필터 초기화 기능 추가

--- src/components/common/ListBox.tsx
+++ src/components/common/ListBox.tsx
@@ -2,14 +2,14 @@ import {Box} from './Box';
 import {css} from '@emotion/react';
 import React, {FC} from 'react';
 import {Hammer} from './Hammer';
+import Typography from './Typography/Typography';
 
 type Props = {
   title: string;
-  list?: {text: string; subText: string}[];
-  onClick?: (value: string) => void;
+  list: {text: string; subText: number}[];
 };
 
-export const ListBox: FC<Props> = ({title, list, onClick}) => {
+export const ListBox: FC<Props> = ({title, list}) => {
   return (
     <Box
       style={css`
@@ -20,46 +20,43 @@ export const ListBox: FC<Props> = ({title, list, onClick}) => {
         padding: 1.5rem;
         border-radius: 2.5rem;
       `}>
-      <h3
+      <div
         css={css`
-          text-align: center;
-          font-size: 1.125rem;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
         `}>
-        {title}
-      </h3>
+        <Typography component="h3" variant="medium">
+          {title}
+        </Typography>
+      </div>
       {list ? (
         <div
           css={css`
             display: flex;
             flex-direction: column;
             justify-content: flex-start;
-            row-gap: 0.5rem;
             flex-grow: 1;
           `}>
           {list.map(({text, subText}) => (
             <div
-              onClick={() => onClick?.(text)}
+              key={text}
               css={css`
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
+                cursor: pointer;
+                &:hover {
+                  text-decoration: underline;
+                }
               `}>
-              <p
-                css={css`
-                  font-size: 1rem;
-                  cursor: pointer;
-                  &:hover {
-                    text-decoration: underline;
-                  }
-                `}>
+              <Typography component="span" variant="p">
                 {text}
-              </p>
-              <p
-                css={css`
-                  font-size: 0.8rem;
-                `}>
+              </Typography>
+              <Typography component="span" variant="weak">
                 {subText}
-              </p>
+              </Typography>
             </div>
           ))}
         </div>
