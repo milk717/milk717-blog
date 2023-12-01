49c9f93b031a9210f1b8e81c73550a92e476cf5b
Sumin <robolindasoo@gmail.com>
2023-12-01 11:00:40 +0900

    :lipstick: 제목 태그별 알맞은 사이즈 할다

--- src/styles/theme/_fontSizes.ts
+++ src/styles/theme/_fontSizes.ts
@@ -2,9 +2,9 @@ export const fontSizes = {
   xs: `0.8rem`,
   sm: `0.9rem`,
   base: `1rem`,
-  md: `1.125rem`,
+  md: `1.1rem`,
   lg: `1.25rem`,
   xl: `1.4rem`,
-  xxl: `1.5rem`,
-  xxxl: `2rem`,
+  xxl: `1.8rem`,
+  xxxl: `2.25rem`,
 };
