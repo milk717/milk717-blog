32a73ffb73524ec89445173ba051d6a07649d443
Sumin <robolindasoo@gmail.com>
2023-12-09 00:57:11 +0900

    :mag: 블로그 포스트에 meta 헤드 추가

--- src/pages/{mdx.frontmatter__slug}.tsx
+++ src/pages/{mdx.frontmatter__slug}.tsx
@@ -29,6 +29,34 @@ const BlogPostTemplate = ({
 };
 export default BlogPostTemplate;
 
+export const Head = ({data}: PageProps<Queries.BlogPostQuery>) => {
+  const mdx = data.mdx;
+  if (!mdx || !mdx.frontmatter || !mdx.excerpt) return null;
+  return (
+    <>
+      <title>{mdx.frontmatter.title}</title>
+      <meta name="author" content="milk717" />
+      <meta name="description" content={mdx.excerpt} />
+
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content={`https://www.milk717.com/${mdx.frontmatter.slug}`}
+      />
+      <meta property="og:title" content={mdx.frontmatter.title!} />
+      <meta property="og:description" content={mdx.excerpt} />
+      <meta
+        property="og:image"
+        content={
+          mdx.frontmatter.thumbnail?.childrenImageSharp?.at(0)?.original?.src!
+        }
+      />
+      <meta property="og:site_name" content={mdx.frontmatter.title!} />
+      <meta property="og:locale" content="ko_KR" />
+    </>
+  );
+};
+
 export const pageQuery = graphql`
   query BlogPost($id: String!) {
     mdx(id: {eq: $id}) {
@@ -37,7 +65,16 @@ export const pageQuery = graphql`
         category
         tags
         date
+        slug
+        thumbnail {
+          childrenImageSharp {
+            original {
+              src
+            }
+          }
+        }
       }
+      excerpt
     }
   }
 `;
